3eee425cf747345b4c40efe8ba50129a2c3801f8
Luis Capelo <luiscape@gmail.com>
2014-02-06 20:26:02 -0500

    Initial commit to the search function.

---
+++ fts.search.R
@@ -0,0 +1,8 @@
+#### Function to search the crisis id in FTS -- necesary for fetching the data from its API. ####
+
+
+fts.search <- function() {
+  # Fetch a list of crisis (start with current if easier). Then parse the list looking for the 
+  # name of a country. 
+  # Should use iso3 code?
+}
